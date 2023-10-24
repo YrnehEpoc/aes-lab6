1c8415accf2316de422738679e8795524d15d197
Henry Cope <henrydaviscope@gmail.com>
2023-10-23 21:37:12 -0600

    working on competing test, hang during testing

--- test/test_compete/test_compete.c
+++ test/test_compete/test_compete.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <zephyr.h>
 #include <unity.h>
-#include "scheduler.h"
+#include "compete.h"
 
 #define STACKSIZE 2000
 #define HIPRIDELAY 1000
@@ -29,8 +29,8 @@ void tearDown(void)
 
 }
 
-void coop_busy_busy_equal(void){
-    char[] myName = "Henry";
+void preempt_busy_busy_equal(void){
+    char myName[] = "Henry";
     
     for (int t = 0; t < THREAD_COUNT; t++) {
         if (t == 0){
@@ -41,9 +41,12 @@ void coop_busy_busy_equal(void){
                         &myName, NULL, NULL,
                         K_PRIO_COOP(0),
                         0,
-                        0);
+                        K_NO_WAIT);
         }
     }
+    k_thread_runtime_stats_t rt_stats_thread;
+    k_thread_runtime_stats_get(k_current_get(),&rt_stats_thread);
+    printk("Cycles: %llu\n", rt_stats_thread.execution_cycles);
 }
 
 
@@ -52,6 +55,6 @@ int main (void)
     
     UNITY_BEGIN();
 
-    RUN_TEST(test_grab);
+    RUN_TEST(preempt_busy_busy_equal);
     return UNITY_END();
 }
