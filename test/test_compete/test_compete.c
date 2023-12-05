bc78b852a04ab73907896e8f90faea67500d219f
Henry <henrydaviscope@gmail.com>
2023-12-04 19:34:56 -0700

    reworked scheduler, working on test_compete

--- test/test_compete/test_compete.c
+++ test/test_compete/test_compete.c
@@ -3,17 +3,20 @@
 #include <unity.h>
 #include "compete.h"
 
-#define STACKSIZE 2000
-#define HIPRIDELAY 1000
-#define THREAD_COUNT 2
+#define STACKSIZE 2000 
+ 
 
-struct k_thread supervisor_thread;
+ 
+struct k_thread thread1, thread2, supervisor;
+
+
+
+
 K_THREAD_STACK_DEFINE(supervisor_stack, STACKSIZE);
+K_THREAD_STACK_DEFINE(thread1_stack, STACKSIZE);
+K_THREAD_STACK_DEFINE(thread2_stack, STACKSIZE);
 
-struct k_thread worker_threads[THREAD_COUNT];
-K_THREAD_STACK_ARRAY_DEFINE(worker_stacks, THREAD_COUNT, STACKSIZE);
 
-int takenNum = 0;
 
 void setUp(void)
 {
@@ -22,33 +25,70 @@ void setUp(void)
 
 void tearDown(void)
 {
-    
-    for (int t = 0; t < THREAD_COUNT; t++) {
-        k_thread_abort(&worker_threads[t]);
-    }
 
 }
 
-void preempt_busy_busy_equal(void){
-    //add metaIRQ for cancelling stuck threads
-    char myName[] = "Henry";
-    k_thread_runtime_stats_t init_stats;
-    k_thread_runtime_stats_all_get(&init_stats);
-    for (int t = 0; t < THREAD_COUNT; t++) {
-        if (t == 0){
-        k_thread_create(&worker_threads[t],
-                        worker_stacks[t],
-                        STACKSIZE,
-                        (k_thread_entry_t) busy_busy,
-                        &myName, NULL, NULL,
-                        K_PRIO_COOP(0),
-                        0,
-                        K_NO_WAIT);
-        }
-    }
+
+void super_entry()
+{
+    printk("Suspending threads\n");
+    k_thread_suspend(&thread1);
+    k_thread_suspend(&thread2);
+}
+
+void run_threads(
+    char* test,
+    k_thread_entry_t t1_entry, int t1_prio,
+    k_thread_entry_t t2_entry, int t2_prio
+)
+{
+    k_thread_runtime_stats_t thread1_rt_stats, thread2_rt_stats; 
+
+    k_thread_create(&supervisor,
+                    supervisor_stack,
+                    STACKSIZE,
+                    (k_thread_entry_t) super_entry,
+                    NULL, NULL, NULL,
+                    -CONFIG_NUM_COOP_PRIORITIES,
+                    0,
+                    K_MSEC(75)); 
     
-    //k_thread_runtime_stats_get(k_current_get(),&rt_stats_thread);
-    printk("Cycles: %llu\n", init_stats);
+    
+    k_thread_create(&thread1,
+                    thread1_stack,
+                    STACKSIZE,
+                    (k_thread_entry_t) t1_entry,
+                    NULL, NULL, NULL,
+                    t1_prio,
+                    0,
+                    K_MSEC(10)); 
+    
+    
+    k_thread_create(&thread2,
+                    thread2_stack,
+                    STACKSIZE,
+                    (k_thread_entry_t) t2_entry,
+                    NULL, NULL, NULL,
+                    t2_prio,
+                    0,
+                    K_MSEC(15)); 
+    
+    k_thread_join(&supervisor, K_MSEC(150));
+    
+    k_thread_runtime_stats_get(&thread1, &thread1_rt_stats);
+    k_thread_runtime_stats_get(&thread2, &thread2_rt_stats);
+    k_thread_abort(&thread1);
+    k_thread_abort(&thread2);
+    printk("Test: %s\nThread 1 cycles: %llu\nThread 2 cycles: %llu\n", test, thread1_rt_stats.execution_cycles, thread2_rt_stats.execution_cycles);
+}
+
+
+
+void preempt_busy_busy_equal(void){ 
+    run_threads("preempt_busy_busy_equal",
+        busy_busy, K_PRIO_PREEMPT(1),
+        busy_busy, K_PRIO_PREEMPT(1)
+    );
 }
 
 
@@ -58,5 +98,16 @@ int main (void)
     UNITY_BEGIN();
 
     RUN_TEST(preempt_busy_busy_equal);
+    // RUN_TEST(coop_equal_busy_busy); 
+    // RUN_TEST(coop_equal_busy_yield);
+    // RUN_TEST(preempt_equal_busy_yield);
+    // RUN_TEST(coop_high_busy_busy);
+    // RUN_TEST(coop_low_busy_busy);
+    // RUN_TEST(coop_high_busy_yield);
+    // RUN_TEST(coop_low_busy_yield);
+    // RUN_TEST(preempt_high_busy_busy);
+    // RUN_TEST(preempt_low_busy_busy);
+    // RUN_TEST(preempt_high_busy_yield);
+    // RUN_TEST(preempt_low_busy_yield);
     return UNITY_END();
 }
--- test/test_schedule/test_scheduler.c
+++ test/test_schedule/test_scheduler.c
@@ -3,69 +3,59 @@
 #include <unity.h>
 #include "scheduler.h"
 
-#define STACKSIZE 2000
-#define HIPRIDELAY 1000
-#define THREAD_COUNT 2
+#define STACKSIZE 2000  
 
-struct k_thread worker_threads[THREAD_COUNT];
-K_THREAD_STACK_ARRAY_DEFINE(worker_stacks, THREAD_COUNT, STACKSIZE);
+struct k_thread thread1;
+struct k_thread thread2;
+K_THREAD_STACK_DEFINE(thread1_stack, STACKSIZE);
+K_THREAD_STACK_DEFINE(thread2_stack, STACKSIZE);
 
 int takenNum = 0;
 
 void setUp(void)
 {
 
+}
+
+void test_schedule(void){
+
     struct k_sem semaphore;
     k_sem_init(&semaphore, 0, 1);
+ 
+    k_thread_create(&thread1,
+                    thread1_stack,
+                    STACKSIZE,
+                    (k_thread_entry_t) sema_grabber_thread,
+                    &semaphore, 1, &takenNum,
+                    K_PRIO_PREEMPT(1),
+                    0,
+                    K_MSEC(15)); 
 
-    for (int t = 0; t < THREAD_COUNT; t++) {
-        if (t == 0){
-        k_thread_create(&worker_threads[t],
-                        worker_stacks[t],
-                        STACKSIZE,
-                        (k_thread_entry_t) sema_grabber_thread,
-                        &semaphore, t, &takenNum,
-                        K_PRIO_COOP(0),
-                        0,
-                        K_MSEC(HIPRIDELAY));
-        }
-        else
-        {
-        k_thread_create(&worker_threads[t],
-                        worker_stacks[t],
-                        STACKSIZE,
-                        (k_thread_entry_t) sema_grabber_thread,
-                        &semaphore, t, &takenNum,
-                        K_PRIO_COOP(1),
-                        0,
-                        K_NO_WAIT);
-        }
-    }
+    k_thread_create(&thread2,
+                    thread2_stack,
+                    STACKSIZE,
+                    (k_thread_entry_t) sema_grabber_thread,
+                    &semaphore, 2, &takenNum,
+                    K_PRIO_PREEMPT(2),
+                    0,
+                    K_NO_WAIT);  
 
-    
-
+    TEST_ASSERT_EQUAL(1,takenNum);
 }   
 
 void tearDown(void)
 {
-    
-    for (int t = 0; t < THREAD_COUNT; t++) {
-        k_thread_abort(&worker_threads[t]);
-        printk("Made it here");
-    }
+     
 
 }
-
-void test_grab(void){
-    TEST_ASSERT_EQUAL(1,takenNum);
-}
+ 
 
 int main (void)
 {
     
     UNITY_BEGIN();
 
-    RUN_TEST(test_grab);
+    RUN_TEST(test_schedule);
     printk("made it to this point");
     return UNITY_END();
 }
